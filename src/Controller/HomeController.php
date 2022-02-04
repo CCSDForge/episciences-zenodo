3159a3848dd54c7d6b943fc5e8403642e6891fe5
JulienC <julien.charles@ccsd.cnrs.fr>
2022-02-04 09:35:00 +0100

    Cas integration to inter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("{_locale<en|fr>}/home", name="home")
      */
     public function index(): Response
     {
